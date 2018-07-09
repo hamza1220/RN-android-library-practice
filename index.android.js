6cf8d1764517fba8260840641ec294cf087d7b96
hamza1220 <hamzaahmad86@gmail.com>
2018-07-09 19:04:05 +0500

    First commit

--- index.android.js
+++ index.android.js
@@ -4,11 +4,4 @@ import { NativeModules } from 'react-native'
 // name as defined via ReactContextBaseJavaModule's getName
 
 
-
-@Override
-public String getName() {
-    return "sampleLibrary";
-}
-
-
 module.exports = NativeModules.sampleLibrary
